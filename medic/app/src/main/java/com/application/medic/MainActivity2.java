b0e033959946e4d7bf927a66de8f21a6e314d92a
nimbh <inshina.2005@mail.ru>
2023-04-25 21:56:02 +0200

    Initial commit

---
+++ medic/app/src/main/java/com/application/medic/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.application.medic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void addAnalys(View v){
+        v.setBackgroundColor(Color.WHITE);
+        
+    }
+}
